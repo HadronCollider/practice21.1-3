3f54d28467cce88cffa0cb86e3802b71546dabdb
Yue2u <beloleg225@mail.ru>
2021-06-06 18:37:12 +0700

    Testing networking

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -28,15 +28,15 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this,
             LinearLayoutManager.HORIZONTAL, false)
 
-        lifecycleScope.launch(Dispatchers.Main) {
-            val list = generateList()
+        //lifecycleScope.launch(Dispatchers.Main) {
+            //val list = generateList()
 
             recyclerView.adapter = MyRecyclerViewAdapter(
-                list,
+                dI(),
                 getScreenSize(),
                 lifecycleScope
             ) // TODO pass lifecycleScope + add .withLoadStateFooter(FootAdapter)
-        }
+        //}
     }
 
     private suspend fun generateList(): List<Manga> {
@@ -63,6 +63,33 @@ class MainActivity : AppCompatActivity() {
         }
         return list
     }
+
+
+    private fun dI(): List<Manga> {
+        val list = mutableListOf<Manga>()
+
+        val urls: MutableList<String> = mutableListOf(
+            "https://api.remanga.org/media/titles/military/9539534ce2ea8e6c12b4d1d81debe59f.jpg",
+            "https://get.wallhere.com/photo/hill-with-blue-river-gorgeous-beauty-of-mountain-1550789.jpg",
+            "https://w-dog.ru/wallpapers/9/1/298499341842887/osen-vodopady-kanchanaburi-provinciya-priroda-foto.jpg",
+            "https://i.imgur.com/9I23DL3.jpg",
+            "https://s1.1zoom.ru/b5050/994/260356-svetik_1920x1200.jpg",
+            "https://c.wallhere.com/photos/ab/98/lake_round_coast_sky_mountains-1064574.jpg!d",
+            "https://wallpapertag.com/wallpaper/full/3/b/a/522858-butterfly-desktop-backgrounds-1920x1200-photos.jpg",
+            //"https://www.wallpaperup.com/uploads/wallpapers/2016/10/14/1025923/403030823b8c642fbab1fe0305865aca-1000.jpg",
+            "https://c.wallhere.com/photos/40/80/1920x1200_px_bridges_forest_landscapes_Morning_nature_rivers_scenic-1783173.jpg!d",
+            "https://c.wallhere.com/photos/d3/be/pond_geese_lodges_mill_wheel_summer-1054309.jpg!d",
+        )
+        lifecycleScope.launch(Dispatchers.IO) {
+            for (i in 0 until 9) {
+                list.add(
+                    Manga("manga №$i", "Any genre", urls[i], Picasso.get().load(urls[i]).get())
+                )
+            }
+        }
+        return list
+    }
+
 
     private fun getScreenSize(): ImageSize{
         val displayMetrics = DisplayMetrics()
