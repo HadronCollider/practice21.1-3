6860d40bb5efae3a33b4d7392ad6edb420c6a7aa
Yue2u <beloleg225@mail.ru>
2021-06-07 13:39:29 +0700

    Downloading preview images taken out of adapter

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,23 +1,21 @@
 package com.makentoshe.androidgithubcitemplate
 
 
-import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.util.Log
-import android.view.WindowManager
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,18 +26,15 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this,
             LinearLayoutManager.HORIZONTAL, false)
 
-        //lifecycleScope.launch(Dispatchers.Main) {
-            //val list = generateList()
 
-            recyclerView.adapter = MyRecyclerViewAdapter(
-                dI(),
-                getScreenSize(),
-                lifecycleScope
-            ) // TODO pass lifecycleScope + add .withLoadStateFooter(FootAdapter)
-        //}
+        getMangas{mangas ->
+            lifecycleScope.launch(Dispatchers.Main){
+                recyclerView.adapter = MyRecyclerViewAdapter(mangas)
+            }
+        }
     }
 
-    private suspend fun generateList(): List<Manga> {
+    /*private suspend fun generateList(): List<Manga> {
         val list = mutableListOf<Manga>()
 
         val urls: MutableList<String> = mutableListOf(
@@ -62,36 +57,35 @@ class MainActivity : AppCompatActivity() {
                 Manga("manga №$i", "Any genre", urls[i], image.await()))
         }
         return list
-    }
+    }*/
 
-
-    private fun dI(): List<Manga> {
-        val list = mutableListOf<Manga>()
-
-        val urls: MutableList<String> = mutableListOf(
-            "https://api.remanga.org/media/titles/military/9539534ce2ea8e6c12b4d1d81debe59f.jpg",
-            "https://get.wallhere.com/photo/hill-with-blue-river-gorgeous-beauty-of-mountain-1550789.jpg",
-            "https://w-dog.ru/wallpapers/9/1/298499341842887/osen-vodopady-kanchanaburi-provinciya-priroda-foto.jpg",
-            "https://i.imgur.com/9I23DL3.jpg",
-            "https://s1.1zoom.ru/b5050/994/260356-svetik_1920x1200.jpg",
-            "https://c.wallhere.com/photos/ab/98/lake_round_coast_sky_mountains-1064574.jpg!d",
-            "https://wallpapertag.com/wallpaper/full/3/b/a/522858-butterfly-desktop-backgrounds-1920x1200-photos.jpg",
-            //"https://www.wallpaperup.com/uploads/wallpapers/2016/10/14/1025923/403030823b8c642fbab1fe0305865aca-1000.jpg",
-            "https://c.wallhere.com/photos/40/80/1920x1200_px_bridges_forest_landscapes_Morning_nature_rivers_scenic-1783173.jpg!d",
-            "https://c.wallhere.com/photos/d3/be/pond_geese_lodges_mill_wheel_summer-1054309.jpg!d",
-        )
-        lifecycleScope.launch(Dispatchers.IO) {
-            for (i in 0 until 9) {
-                list.add(
-                    Manga("manga №$i", "Any genre", urls[i], Picasso.get().load(urls[i]).get())
-                )
+    private fun getMangas(after: (mangas: List<BitmapMangaWrapper>) -> Unit){
+        val list = getMangasList()
+        lifecycleScope.launch(Dispatchers.IO){
+            val mangas = list.map{ manga ->
+                val response = client.newCall(Request.Builder().url(manga.imageUrl).build()).execute()
+                val bitmap = BitmapFactory.decodeStream(response.body?.byteStream())
+                BitmapMangaWrapper(manga, bitmap)
             }
+            after.invoke(mangas)
         }
-        return list
     }
 
 
-    private fun getScreenSize(): ImageSize{
+    private fun getMangasList(): List<Manga> = listOf(
+        "https://api.remanga.org/media/titles/military/9539534ce2ea8e6c12b4d1d81debe59f.jpg",
+        "https://get.wallhere.com/photo/hill-with-blue-river-gorgeous-beauty-of-mountain-1550789.jpg",
+        "https://w-dog.ru/wallpapers/9/1/298499341842887/osen-vodopady-kanchanaburi-provinciya-priroda-foto.jpg",
+        "https://i.imgur.com/9I23DL3.jpg",
+        "https://s1.1zoom.ru/b5050/994/260356-svetik_1920x1200.jpg",
+        "https://c.wallhere.com/photos/ab/98/lake_round_coast_sky_mountains-1064574.jpg!d",
+        "https://wallpapertag.com/wallpaper/full/3/b/a/522858-butterfly-desktop-backgrounds-1920x1200-photos.jpg",
+        "https://c.wallhere.com/photos/40/80/1920x1200_px_bridges_forest_landscapes_Morning_nature_rivers_scenic-1783173.jpg!d",
+        "https://c.wallhere.com/photos/d3/be/pond_geese_lodges_mill_wheel_summer-1054309.jpg!d"
+    ).mapIndexed { index, url -> Manga("manga №$index", "Any genre", url) }
+
+
+    /*private fun getScreenSize(): ImageSize{
         val displayMetrics = DisplayMetrics()
         val windowsManager = applicationContext.getSystemService(WINDOW_SERVICE) as WindowManager
 
@@ -99,7 +93,7 @@ class MainActivity : AppCompatActivity() {
         Log.d("SCREEN_METRICS", "${displayMetrics.widthPixels}x${displayMetrics.heightPixels}")
 
         return ImageSize(displayMetrics.widthPixels, displayMetrics.heightPixels)
-    }
+    }*/
 
 }
 
--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MyRecyclerViewAdapter.kt
@@ -1,25 +1,16 @@
 package com.makentoshe.androidgithubcitemplate
 
 import android.graphics.Bitmap
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
-import kotlinx.coroutines.*
-import java.util.concurrent.CopyOnWriteArrayList
 
 
-class MyRecyclerViewAdapter(private val data: List<Manga>,
-                            private val screenSize: ImageSize,
-                            private val  lifecycleCoroutineScope: LifecycleCoroutineScope) :
+class MyRecyclerViewAdapter(private val data: List<BitmapMangaWrapper>) :
 RecyclerView.Adapter<MyRecyclerViewAdapter.MyViewHolder>(){
-
-    private val mangaImages: CopyOnWriteArrayList<Bitmap?> = generateEmptyCaches()
 
     //class that handle single element of recycler view
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
@@ -35,12 +26,9 @@ RecyclerView.Adapter<MyRecyclerViewAdapter.MyViewHolder>(){
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
 
-        holder.mangaTitle.text = data[position].title
-        holder.mangaGenre.text = data[position].genre
-        if(mangaImages[position] == null){
-            mangaImages[position] = getNewImage(data[position].image, screenSize)
-        }
-        holder.mangaPreviewImage.setImageBitmap(mangaImages[position])
+        holder.mangaTitle.text = data[position].manga.title
+        holder.mangaGenre.text = data[position].manga.genre
+        holder.mangaPreviewImage.setImageBitmap(data[position].image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -52,19 +40,12 @@ RecyclerView.Adapter<MyRecyclerViewAdapter.MyViewHolder>(){
         return MyViewHolder(itemView)
     }
 
-    private fun getNewImage(image: Bitmap, screenSize: ImageSize, imagesOnScreen: Double = 2.5) : Bitmap{
+    /*private fun getNewImage(image: Bitmap, screenSize: ImageSize, imagesOnScreen: Double = 2.5) : Bitmap{
         val newImageWidth = (screenSize.width.toDouble() / imagesOnScreen - screenSize.width.toDouble() * 0.05).toInt()
         val resizer = ImageResizer(image, newImageWidth) // 0 passed to save image ratio
 
         return resizer.getResizedImage()
-    }
+    }*/
 
-    private fun generateEmptyCaches(): CopyOnWriteArrayList<Bitmap?> {
-        val result = CopyOnWriteArrayList<Bitmap?>()
-        for(i in data){
-            result.add(null)
-        }
-        return result
-    }
 
 }
