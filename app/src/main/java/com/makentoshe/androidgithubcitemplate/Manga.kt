ec843d8af304c6ed25ae9122a1a63440f291bf0d
Yue2u <beloleg225@mail.ru>
2021-06-07 13:36:45 +0700

    Added wrapper class for Manga that store bitmap preview image of manga

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/Manga.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/Manga.kt
@@ -2,7 +2,7 @@
 package com.makentoshe.androidgithubcitemplate
 
 import android.graphics.Bitmap
-import android.media.Image
-import kotlinx.coroutines.coroutineScope
 
-data class Manga(val title: String, val genre: String, val imageUrl: String, val image: Bitmap) {}
+
+data class Manga(val title: String, val genre: String, val imageUrl: String)
+data class BitmapMangaWrapper(val manga: Manga, val image: Bitmap)
