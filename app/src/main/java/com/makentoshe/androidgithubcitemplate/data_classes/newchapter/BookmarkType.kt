76d2f41fb9e189405bc65e39aa8a951cfbfbb585
alinalimac <1977oleg@gmail.com>
2021-06-08 23:49:58 +0300

    Revert "Data classes for "New chapters" panel created"
    This reverts commit b1e92b6a

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/data_classes/newchapter/BookmarkType.kt
+++
@@ -1,9 +0,0 @@
-package com.makentoshe.androidgithubcitemplate.data_classes.newchapter
-import com.google.gson.annotations.SerializedName
-
-data class BookmarkType(
-    @SerializedName("id")
-    val id: Int,
-    @SerializedName("name")
-    val name: String
-)
