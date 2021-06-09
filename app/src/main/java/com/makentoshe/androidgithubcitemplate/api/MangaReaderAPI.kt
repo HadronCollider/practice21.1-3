5f021d26c9e8ac2187946d3a4e3ff2862973d78d
alinalimac <1977oleg@gmail.com>
2021-06-09 13:18:18 +0300

    GET function for comment section added to MangaReaderAPI.kt

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/api/MangaReaderAPI.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/api/MangaReaderAPI.kt
@@ -18,4 +18,14 @@ interface MangaReaderAPI {
         @Path("manga_name")
         manga_name: String?
     ): Call<ResponseBody>
+
+    @GET("/api/activity/comments/") // /api/activity/comments/?title_id=45666&page=1&ordering=-id
+    fun getComments(
+        @Query("title_id")
+        title_id: Int,     //every manga has a special id number
+        @Query("page")
+        page: Int = 1,
+        @Query("ordering")
+        ordering: String = "-id"
+    ): Call<ResponseBody>
 }
